8d7dbc26a3173150f8b925511317fbb0db53caa5
d-rechun <33349566+d-rechun@users.noreply.github.com>
2018-01-20 18:12:11 +0100

    Hello World update

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -50,33 +50,6 @@ int main ()
     fooray(array);
     for (int o = 0; array[o] != -1; o++)
         cout << "Array: " << array[o] << endl;
-    /*
-    vector<int> myvector (3,100);
-    vector<int>::iterator it;
-
-    it = myvector.begin();
-    it = myvector.insert ( it , 200 );
-
-  myvector.insert (it,2,300);
-
-  std::cout << "Test:";
-  for (it=myvector.begin(); it<myvector.end(); it++)
-    std::cout << ' ' << *it;
-  std::cout << '\n';
-
-  // "it" no longer valid, get a new one:
-  it = myvector.begin();
-
-  std::vector<int> anothervector (2,400);
-  myvector.insert (it+2,anothervector.begin(),anothervector.end());
-
-  int myarray [] = { 501,502,503 };
-  myvector.insert (myvector.begin(), myarray, myarray+3);
-
-  std::cout << "myvector contains:";
-  for (it=myvector.begin(); it<myvector.end(); it++)
-    std::cout << ' ' << *it;
-  std::cout << '\n';*/
 
   return 0;
 }
