8ef0488a2a0e359a24e907e74427e7e7ae486bc1
d-rechun <33349566+d-rechun@users.noreply.github.com>
2018-01-20 17:20:34 +0100

    Update main.cpp

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -28,7 +28,8 @@ int main ()
     vector<int>::iterator it;
     for(int i = 0; i < 11; i++)
         cout << "Hello World!" << endl;
-
+    for(int i = 0; i < 5; i++)
+        cout << "Bey World!" << endl;
     for(int i = 0; i < 10; i++)
         testvector.push_back(i);
     testvector = foo(testvector);
